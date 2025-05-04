6b6e806392bf7f167de584cbe50a161a5ab2eb80
Kirill <kiw197204@gmail.com>
2025-05-04 21:22:43 +0400

    feat: find home dir and up command

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import process from "node:process"
+import process, { chdir } from "node:process"
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import readline from 'node:readline'
+import os from 'node:os'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const homeDir = os.homedir();
+
+chdir(homeDir)
+
+const dirMsg = () => console.log(`You are currently in ${process.cwd()}`)
 
 const username = process.argv.find(
   el => el.startsWith('--username')
 )?.split('=')[1] ?? 'Anon'
 console.log(`Welcome to the File Manager, ${username}!`)
+dirMsg()
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -28,6 +35,8 @@ rl.on('line', (line) => {
   const input = line.split(' ')
   const args = input.slice(1)
   const cmd = input[0]
-  console.log(cmd)
-  console.log(args)
+  if (cmd === 'up') {
+    chdir('..')
+  }
+  dirMsg()
 })
