794f32331febab08fd3b28f3cd4503044a1539cc
Kirill <kiw197204@gmail.com>
2025-05-04 20:55:21 +0400

    feat: say hi

---
+++ index.js
@@ -0,0 +1,11 @@
+import process from "node:process"
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const username = process.argv.find(
+  el => el.startsWith('--username')
+)?.split('=')[1] ?? 'Anon'
+console.log(`Welcome to the File Manager, ${username}!`)
