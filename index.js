7cb80844d73ca33557263be8a8743949e4d48b5d
Kirill <kiw197204@gmail.com>
2025-05-04 21:08:49 +0400

    feat: read and write commands, say goodbye

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import process from "node:process"
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import readline from 'node:readline'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -9,3 +10,24 @@ const username = process.argv.find(
   el => el.startsWith('--username')
 )?.split('=')[1] ?? 'Anon'
 console.log(`Welcome to the File Manager, ${username}!`)
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+})
+
+process.on('SIGINT', () => {
+  rl.close()
+})
+
+rl.on('line', (line) => {
+  const input = line.split(' ')
+  const args = input.slice(1)
+  const cmd = input[0]
+  console.log(cmd)
+  console.log(args)
+})
